2f5f6103b49d07db484891b02a58e0835c8ea02f
Patrick Dolan <dolanp1992@gmail.com>
2021-12-10 20:35:39 -0800

    add calculatePizzaPrice prototype for Pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,19 @@ Pizza.prototype.addToppings = function(toppings) {
 Pizza.prototype.calculateToppingPrice = function() {
   this.toppingPrice = this.toppings.length;
 };
+Pizza.prototype.calculatePizzaPrice = function() {
+  (isNaN(this.toppingPrice) ? this.pizzaPrice = 0 : this.pizzaPrice = this.toppingPrice);
+  switch (this.size) {
+    case "small":
+      return this.pizzaPrice += 10;
+    case "medium":
+      return this.pizzaPrice += 12;
+    case "large":
+      return this.pizzaPrice += 14;
+    case "x-large":
+      return this.pizzaPrice += 16;
+  }
+};
 //Global Variables
 let order = new Order;
 let pizza1 = new Pizza("small");
